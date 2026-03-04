28fadf175dc6cb6ef81b0a16a9f41d7ab409546e
AtomicZer0 <joaoas.joaoas@gmail.com>
2026-03-04 20:20:10 -0300

    Atualização - Task Service

--- task-manager-backend/src/tasks/tasks.service.ts
+++ task-manager-backend/src/tasks/tasks.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectRepository(Task) private repo: Repository<Task>) {}
+
+  create(dto: CreateTaskDto): Promise<Task> {
+    const task = this.repo.create(dto);
+    return this.repo.save(task);
+  }
+
+  findAll(): Promise<Task[]> {
+    return this.repo.find();
+  }
+
+  async findOne(id: string): Promise<Task> {
+    const task = await this.repo.findOneBy({ id });
+    if (!task) throw new NotFoundException(`ERRO 404: Task ${id} não encontrada`);
+    return task;
+  }
+
+  async update(id: string, dto: UpdateTaskDto): Promise<Task> {
+    const task = await this.findOne(id);
+    Object.assign(task, dto);
+    return this.repo.save(task);
+  }
+
+  async remove(id: string): Promise<void> {
+    const task = await this.findOne(id);
+    await this.repo.remove(task);
+  }
+}
